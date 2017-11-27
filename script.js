4ddde9aa9a0da83a6d3de7af02ff803dd9ad862e
Anwana Ntofon <aontofon@ucdavis.edu>
2017-11-27 12:10:10 -0800

    Acquired JSON from Weather API and converted celsius value to farenheit. Button does not yet convert the temp values

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
   var farenheit = "F";
   var curTempScale = $("#temp-scale").text();
   var oppositeTempScale = $("#opposite-temp-scale").text();
+  var opencageAPI = '';
+  var weatherAPI = '';
+  var convertToCelsius = 0;
+  var convertToFarenheit = 0;
   var lat = 0;
   var long = 0;
 
@@ -23,68 +27,48 @@ $(document).ready(function() {
       oppositeTempScale = farenheit;
       $("#opposite-temp-scale").text(oppositeTempScale);
     }
-});
+  });
 
-/* === USE OF WEATHER API BELOW === */
-$.ajax({
-  url: 'https://fcc-weather-api.glitch.me/api/current?lon=:longitude&lat=:latitude',
-  type: 'GET',
-  dataType: 'json',
-  // data: {lon:':longitude', lat:':latitude' }.
-})
-.done(function(json) {
-  console.log("success");
-  // if the user allows access to their location, get their current position which includes longitude and latitude
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+/* === AJAX CALL TO WEATHER & GEOCODER API === */
+  $.ajax({
+    url: 'https://fcc-weather-api.glitch.me/api/current?lon=:longitude&lat=:latitude',
+    type: 'GET',
+    dataType: 'json',
+    // data: {lon:':longitude', lat:':latitude' }.
+  })
+  .done(function(json) {
+    console.log("success");
+    // if the user allows access to their location, get their current position which includes longitude and latitude
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
 
       lat = position.coords.latitude;
       long = position.coords.longitude;
 
+      //build URL, the parameter passed to get the JSON data: https://api.opencagedata.com/geocode/v1/json?q=LAT+LONG&pretty=1&key=<YOURAPIKEY>
+      opencageAPI = 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + long + '&pretty=1&key=80f8fa6078764e978111699fa6732fa4';
+      $.getJSON(opencageAPI, {format:'json'}, function(json) {
+        //results array is returned upon getJSON function call. Subsequent properties shown when traverse in results array
+        $("#location").text(json.results[0].components.city + ", " + json.results[0].components.state);
+      });
 
-      // var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
-      //
-      // $.getJSON(geocoding).done(function(results) {
-      //     alert(results[1]);
-      // });
+      weatherAPI = 'https://fcc-weather-api.glitch.me/api/current?lon=' + long + '&' + 'lat=' + lat;
+      $.getJSON(weatherAPI, {format:'json'}, function(json) {
+        convertToFarenheit = (json.main.temp* 9/5) + 32;
+        $('#temp-value').text(convertToFarenheit);
+        $('#temp-description').text(json.weather[0].description);
+      });
+      });
 
-        opencageAPI = 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + long + '&pretty=1&key=80f8fa6078764e978111699fa6732fa4';
-        // return opencageAPI;
-        $.getJSON(opencageAPI, {format:'json'}, function(json) {
-          $("#location").text(json.results[0].components.city + ", " + json.results[0].components.state);
-        });
-    });
-  }
+    }
 
-  // $.getJSON('https://fcc-weather-api.glitch.me/api/current?lon=:longitude&lat=:latitude', {param1: 'value1'}, function(json) {
-  //   $(".message").html(JSON.stringify(json));
-  //     /*optional stuff to do after success */
-  // });
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
-});
-var opencageAPI = ''
 
-function makeUrl() {
-  opencageAPI = 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + long + '&pretty=1&key=80f8fa6078764e978111699fa6732fa4';
-  // return opencageAPI;
-}
-
-// makeUrl();
-
-// var opencageAPI = 'https://api.opencagedata.com/geocode/v1/json?q=34.690085599999996+-118.18311569999999&pretty=1&key=80f8fa6078764e978111699fa6732fa4';
-// $.getJSON(opencageAPI, {format:'json'}, function(json) {
-//   console.log(lat);
-//   console.log(long);
-//   console.log(opencageAPI);
-//   console.log(json.results[5].components.city + ", " + json.results[5].components.state);
-//   // $(".message").html(JSON.stringify(json));
-//     /*optional stuff to do after success */
-//     // lat: 34.690085599999996 long: -118.18311569999999
-// });
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
 
 });
