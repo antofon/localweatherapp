5df91353a0f676e3f94d2483aab1a91cced8d091
Anwana Ntofon <aontofon@ucdavis.edu>
2017-11-28 18:44:19 -0800

    Accessed image element from Weather API AJAX request and applied it to image element's ID

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   var farenheit = "F";
   var curTempScale = $("#temp-scale").text();
   var oppositeTempScale = $("#opposite-temp-scale").text();
-  var opencageAPI = '';
-  var weatherAPI = '';
+  var opencageURL = '';
+  var weatherURL = '';
   var convertToCelsius = 0;
   var convertToFarenheit = 0;
   var lat = 0;
@@ -42,27 +42,36 @@ $(document).ready(function() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
 
-      lat = position.coords.latitude;
-      long = position.coords.longitude;
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
 
-      //build URL, the parameter passed to get the JSON data: https://api.opencagedata.com/geocode/v1/json?q=LAT+LONG&pretty=1&key=<YOURAPIKEY>
-      opencageAPI = 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + long + '&pretty=1&key=80f8fa6078764e978111699fa6732fa4';
-      $.getJSON(opencageAPI, {format:'json'}, function(json) {
-        //results array is returned upon getJSON function call. Subsequent properties shown when traverse in results array
-        $("#location").text(json.results[0].components.city + ", " + json.results[0].components.state);
-      });
+        //build URL, the parameter passed to get the JSON data: https://api.opencagedata.com/geocode/v1/json?q=LAT+LONG&pretty=1&key=<YOURAPIKEY>
+        opencageURL = 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + long + '&pretty=1&key=80f8fa6078764e978111699fa6732fa4';
+        $.getJSON(opencageURL, {format:'json'}, function(json) {
+          //results array is returned upon getJSON function call. Subsequent properties shown when traverse in results array
+          console.log(json.results[0]);
+          $("#location").text(json.results[0].components.city + ", " + json.results[0].components.state);
+        });
 
-      weatherAPI = 'https://fcc-weather-api.glitch.me/api/current?lon=' + long + '&' + 'lat=' + lat;
-      $.getJSON(weatherAPI, {format:'json'}, function(json) {
-        convertToFarenheit = (json.main.temp* 9/5) + 32;
-        $('#temp-value').text(convertToFarenheit);
-        $('#temp-description').text(json.weather[0].description);
-      });
-      });
+        weatherURL = 'https://fcc-weather-api.glitch.me/api/current?lon=' + long + '&' + 'lat=' + lat;
+        $.getJSON(weatherURL, {format:'json'}, function(json) {
+          convertToFarenheit = (json.main.temp* 9/5) + 32;
 
+          $('#create-temp-image').attr('src', json.weather[0].icon);
+          $('#create-temp-image').attr('width', '75');
+          $('#create-temp-image').attr('height', '75');
+
+          if(convertToFarenheit >= convertToFarenheit + 0.5) {
+            $('#temp-value').text(Math.ceil(convertToFarenheit));
+          }
+
+          else {
+            $('#temp-value').text(Math.floor(convertToFarenheit));
+          }
+          $('#temp-description').text(json.weather[0].description);
+        });
+      });
     }
-
-
   })
   .fail(function() {
     console.log("error");
@@ -70,5 +79,4 @@ $(document).ready(function() {
   .always(function() {
     console.log("complete");
   });
-
 });
